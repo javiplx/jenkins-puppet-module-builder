27552eb02aeeb10b2753c10cfd6b7fe535339644
Javier Palacios <javier.palacios@fon.com>
2015-02-02 17:08:38 +0100

    Skip puppet module building when no changes in push

--- models/puppet.rb
+++ models/puppet.rb
@@ -8,7 +8,18 @@ class PuppetModuleBuilder < Jenkins::Tasks::Builder
 
   def perform(build, launcher, listener)
 
+    env_vars = build.native.environment listener
     puppetdir = build.workspace + 'src' + 'puppet'
+
+    if first = env_vars['GIT_PREVIOUS_SUCCESSFUL_COMMIT'] || env_vars['GIT_PREVIOUS_COMMIT']
+      last = env_vars['GIT_COMMIT']
+      commit_list = StringIO.new
+      launcher.execute('git', 'log', '--oneline' ,"#{first}..${last}", '--', 'src/puppet', {:out => commit_list} )
+      if commit_list.string.lines.empty
+        listener "No new commits under 'src/puppet', skip module build"
+        return
+      end
+    end
 
     # Tree cleanup
     launcher.execute('rm', '-rf', 'pkg', '"="', 'Rakefile', 'spec', {:chdir => puppetdir} )
@@ -28,7 +39,7 @@ class PuppetModuleBuilder < Jenkins::Tasks::Builder
       build_line = build_info.string.lines.find{ |l| l.start_with? 'Module built: ' }.chomp.split
 
       module_file = Pathname.new build_line.last
-      workspace = Pathname.new build.native.environment(listener)['WORKSPACE']
+      workspace = Pathname.new env_vars['WORKSPACE']
 
       artifact_list = { module_file.basename.to_s => module_file.relative_path_from(workspace).to_s }
 
