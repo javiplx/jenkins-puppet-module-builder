43c3c9906e9deae8256457b18a18c79e2e4fb2e5
Javier Palacios <javier.palacios@fon.com>
2015-03-09 11:41:21 +0100

    Create method to retrieve dpkg artifacts

--- models/dpkg_publisher.rb
+++ models/dpkg_publisher.rb
@@ -18,19 +18,28 @@ class DpkgPublisher < Jenkins::Tasks::Publisher
       return
     end
 
-    artifacts = build.native.maven_artifacts
+    file = dpkg_artifacts(build.native)
+    return if file.nil?
+
+    FileUtils.cp file.canonical_path , "/tmp"
+    unless system( "update_repo.py --force /tmp/#{file.name}" )
+      listener.error "Cannot publish #{file.name}"
+      build.native.result = Result.fromString 'FAILURE'
+    end
+
+  end
+
+  private
+
+  def dpkg_artifacts(jenkins_project)
+    artifacts = jenkins_project.maven_artifacts
     return if artifacts.nil?
 
     record = artifacts.module_records.first
     dpkg = record.attachedArtifacts.first
     return if dpkg.nil?
 
-    file = dpkg.getFile(build.native.getModuleLastBuilds.values.first)
-    FileUtils.cp file.canonical_path , "/tmp"
-    unless system( "update_repo.py --force /tmp/#{file.name}" )
-      listener.error "Cannot publish #{file.name}"
-      build.native.result = Result.fromString 'FAILURE'
-    end
+    file = dpkg.getFile(jenkins_project.getModuleLastBuilds.values.first)
   end
 
 end
