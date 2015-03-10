98b2c29f65e1c6b6f520b191329b6cad7b94cc91
Javier Palacios <javier.palacios@fon.com>
2015-03-10 11:59:34 +0100

    Implement snapshots for ant projects

--- models/snapshot_force.rb
+++ models/snapshot_force.rb
@@ -6,11 +6,35 @@ require 'rexml/document'
 class SnapshotForce < Jenkins::Tasks::BuildWrapper
   include Jenkins::Utils
 
-  display_name "(FON) force snapshot on pom"
+  display_name "(FON) force snapshot package versions"
 
   def setup(build, launcher, listener)
 
-    return unless build.native.project.is_a?(Java::HudsonMaven::MavenModuleSet)
+    listener.info "Adding 'SNAPSHOT' to package version"
+
+    if build.native.project.is_a? Java::HudsonMaven::MavenModuleSet
+      return maven_snapshot(build, launcher)
+    end
+
+    buildxml = topdir(build) + 'build.xml'
+    if buildxml.exist?
+      return ant_snapshot(buildxml)
+    end
+
+  end
+
+  private
+
+  def ant_snapshot(buildxml)
+    doc = REXML::Document.new buildxml.read
+    version = doc.root.elements.find{ |e| e.name == 'property' && e.attributes['name'] == 'version' }
+    vers = version.attributes['value'].split('.').collect{ |v| v.to_i }
+    vers[2] += 1
+    version.attributes['value'] = "#{vers.join('.')}~#{Time.now.strftime('%Y%m%d%H%M%S')}"
+    buildxml.native.write(doc.to_s, 'UTF-8')
+  end
+
+  def maven_snapshot(build, launcher)
 
     outstr = StringIO.new
     launcher.execute('git', 'describe', {:out => outstr, :chdir => topdir(build)} )
@@ -18,7 +42,6 @@ class SnapshotForce < Jenkins::Tasks::BuildWrapper
 
     pom = topdir(build).native.child 'pom.xml'
     doc = REXML::Document.new pom.read_to_string
-    listener.info "Adding 'SNAPSHOT' to version declared in pom"
     version = doc.root.elements['version'].text.split('.').collect{ |v| v.to_i }
     version[2] += 1
     doc.root.elements['version'].text = "#{version.join('.')}-SNAPSHOT"
