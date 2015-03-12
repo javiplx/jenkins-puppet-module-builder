0f6a38eb38568b30e5f5e20ad314cb1c2c63ec1a
Javier Palacios <javier.palacios@fon.com>
2015-03-12 13:18:08 +0100

    Add write method to FilePath wrapped object

--- models/snapshot_force.rb
+++ models/snapshot_force.rb
@@ -3,6 +3,14 @@ require 'jenkins/utils'
 require 'stringio'
 require 'rexml/document'
 
+module Jenkins
+  class FilePath
+    def write(string, encoding='UTF-8')
+      @native.write(string, encoding)
+    end
+  end
+end
+
 class SnapshotForce < Jenkins::Tasks::BuildWrapper
   include Jenkins::Utils
 
@@ -31,7 +39,7 @@ class SnapshotForce < Jenkins::Tasks::BuildWrapper
     vers = version.attributes['value'].split('.').collect{ |v| v.to_i }
     vers[2] += 1
     version.attributes['value'] = "#{vers.join('.')}~#{Time.now.strftime('%Y%m%d%H%M%S')}"
-    buildxml.native.write(doc.to_s, 'UTF-8')
+    buildxml.write doc.to_s
   end
 
   def maven_snapshot(build, launcher)
@@ -40,12 +48,12 @@ class SnapshotForce < Jenkins::Tasks::BuildWrapper
     launcher.execute('git', 'describe', {:out => outstr, :chdir => topdir(build)} )
     return if outstr.string.chomp.split('-').length == 1
 
-    pom = topdir(build).native.child 'pom.xml'
-    doc = REXML::Document.new pom.read_to_string
+    pom = topdir(build) + 'pom.xml'
+    doc = REXML::Document.new pom.read
     version = doc.root.elements['version'].text.split('.').collect{ |v| v.to_i }
     version[2] += 1
     doc.root.elements['version'].text = "#{version.join('.')}-SNAPSHOT"
-    pom.write(doc.to_s, 'UTF-8')
+    pom.write doc.to_s
 
   end
 
