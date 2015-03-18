8180954d50312270db03e5dfaaeb349bfd02580d
Javier Palacios <javier.palacios@fon.com>
2015-03-18 12:55:32 +0100

    Unify methods to set snapshots on xml files

--- lib/jenkins/utils.rb
+++ lib/jenkins/utils.rb
@@ -1,14 +1,22 @@
 
 module Jenkins::Utils
 
-  java_import Java.hudson.plugins.git.extensions.impl.RelativeTargetDirectory
+  class FilePath
+    def write(string, encoding='UTF-8')
+      @native.write(string, encoding)
+    end
+  end
 
-  def topdir( build )
-    target = build.native.project.scm.extensions.get RelativeTargetDirectory.java_class
-    if target && local_branch = target.relative_target_dir
-      build.workspace + local_branch
-    else
-      build.workspace
+  module Utils
+    java_import Java.hudson.plugins.git.extensions.impl.RelativeTargetDirectory
+
+    def topdir( build )
+      target = build.native.project.scm.extensions.get RelativeTargetDirectory.java_class
+      if target && local_branch = target.relative_target_dir
+        build.workspace + local_branch
+      else
+        build.workspace
+      end
     end
   end
 
--- models/snapshot_force.rb
+++ models/snapshot_force.rb
@@ -3,14 +3,6 @@ require 'jenkins/utils'
 require 'stringio'
 require 'rexml/document'
 
-module Jenkins
-  class FilePath
-    def write(string, encoding='UTF-8')
-      @native.write(string, encoding)
-    end
-  end
-end
-
 class SnapshotForce < Jenkins::Tasks::BuildWrapper
   include Jenkins::Utils
 
@@ -21,19 +13,25 @@ class SnapshotForce < Jenkins::Tasks::BuildWrapper
     listener.info "Adding 'SNAPSHOT' to package version"
 
     if build.native.project.is_a? Java::HudsonMaven::MavenModuleSet
-      return maven_snapshot(build, launcher)
+      return maven_snapshot(topdir(build)+'pom.xml', listener)
     end
 
     buildxml = topdir(build) + 'build.xml'
     if buildxml.exist?
-      return ant_snapshot(buildxml)
+      return ant_snapshot(buildxml, listener)
     end
 
   end
 
   private
 
-  def ant_snapshot(buildxml)
+  def ant_snapshot(buildxml, listener)
+    launcher = buildxml.create_launcher listener
+
+    outstr = StringIO.new
+    launcher.execute('git', 'describe', {:out => outstr, :chdir => buildxml.parent} )
+    return if outstr.string.chomp.split('-').length == 1
+
     doc = REXML::Document.new buildxml.read
     version = doc.root.elements.find{ |e| e.name == 'property' && e.attributes['name'] == 'version' }
     vers = version.attributes['value'].split('.').collect{ |v| v.to_i }
@@ -42,19 +40,18 @@ class SnapshotForce < Jenkins::Tasks::BuildWrapper
     buildxml.write doc.to_s
   end
 
-  def maven_snapshot(build, launcher)
+  def maven_snapshot(pom, listener)
+    launcher = pom.create_launcher listener
 
     outstr = StringIO.new
-    launcher.execute('git', 'describe', {:out => outstr, :chdir => topdir(build)} )
+    launcher.execute('git', 'describe', {:out => outstr, :chdir => pom.parent} )
     return if outstr.string.chomp.split('-').length == 1
 
-    pom = topdir(build) + 'pom.xml'
     doc = REXML::Document.new pom.read
     version = doc.root.elements['version'].text.split('.').collect{ |v| v.to_i }
     version[2] += 1
     doc.root.elements['version'].text = "#{version.join('.')}-SNAPSHOT"
     pom.write doc.to_s
-
   end
 
 end
